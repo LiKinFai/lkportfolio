2d5429e0d2adc9d15f18a494a2d950ab26c5efd8
LiKinFai <likinfaiemail@gmail.com>
2022-12-06 03:37:02 +0800

    added responsive design

--- src/pages/LKTFTHelper.js
+++ src/pages/LKTFTHelper.js
@@ -1,26 +1,38 @@
 import '.././stylesheets/LKTFTHelper.css'
 
 export function LKTFTHelper() {
-  return <div className='detail-body'>
-    <div className='detail-description'>
-      <img className='detail-photo' alt="LK's TFT Helper" src={require(`../images/lktfthelper2.png`)}></img>
-      <article className='detail-article'>
-        <span className='detail-title'>LK's TFT Helper</span><br></br>
-        This project contains four pages: <b>1.</b> Home page. <b>2.</b> Player Search Page, which allows users to search for a player's information, 
-        and a dedicated proxy server built with <b>Express</b> to handle API requests. <b>3.</b> Odds page, which stores the chances of rolling for every 
-        level in the game TFT. <b>4.</b> Rolling Cost Calculator page, which takes input from users and calculate the cost for rolling for the users
-        in real time.
-      </article>
+  return <div>
+    <div className='detail-body'>
+      <div className='detail-description'>
+        <img className='detail-photo' alt="LK's TFT Helper" src={require(`../images/lktfthelper2.png`)}></img>
+        <article className='detail-article'>
+          <span className='detail-title'>LK's TFT Helper</span><br></br>
+          This project contains four pages: <b>1.</b> Home page. <b>2.</b> Player Search Page, which allows users to search for a player's information, 
+          and a dedicated proxy server built with <b>Express</b> to handle API requests. <b>3.</b> Odds page, which stores the chances of rolling for every 
+          level in the game TFT. <b>4.</b> Rolling Cost Calculator page, which takes input from users and calculate the cost for rolling for the users
+          in real time.
+        </article>
+      </div>
+      <div className='detail-info'>
+        <span className='feature'>Features: </span><span className='listoffeatures'><b>proxy server</b>, <b>hamburger menu</b>, <b>loading spinner</b>, <b>mobile responsive</b>, <b>API fetching</b> ...etc</span><br></br>
+        <span className='related'>Related Tech: </span><span className='techs'>HTML, CSS, Javascript, React, Express</span>
+      </div>
+      <br></br>
+      <div className='sources'>
+          <a href='https://github.com/LiKinFai/lktfthelper' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
+          <a href='https://likinfai.github.io/lktfthelper/#/' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="Page" src={require(`../images/link.png`)}/></a>
+      </div>
     </div>
-    <div className='detail-info'>
-      <span className='feature'>Features: </span><span className='listoffeatures'><b>proxy server</b>, <b>hamburger menu</b>, <b>loading spinner</b>, <b>mobile responsive</b>, <b>API fetching</b> ...etc</span><br></br>
-      <span className='related'>Related Tech: </span><span className='techs'>HTML, CSS, Javascript, React, Express</span>
-    </div>
-    <br></br>
-    <div className='sources'>
-        <a href='https://github.com/LiKinFai/lktfthelper' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
-        <a href='https://likinfai.github.io/lktfthelper/#/' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="Page" src={require(`../images/link.png`)}/></a>
+    <div className='contact' id='contact'>
+    <div className='contact-body'>
+      <header className='contact-header'>Contact Me</header>
+      <div className='contact-buttons'>
+        <a href='https://github.com/LiKinFai' target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
+        <a href='https://www.linkedin.com/in/%E5%81%A5%E8%BC%9D-%E6%9D%8E-195845259/' target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="LinkedIn" src={require(`../images/linkedin.png`)}/></a>
+        <a href="mailto: likinfaiemail@gmail.com" target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="Email" src={require(`../images/email.png`)}/></a>
+      </div>
     </div>
   </div>
+</div>
 
 }
--- src/pages/Sam1Lam4Zuk3EJan1.js
+++ src/pages/Sam1Lam4Zuk3EJan1.js
@@ -1,28 +1,40 @@
 import '.././stylesheets/Sam1Lam4Zuk3EJan1.css'
 
 export function Sam1Lam4Zuk3EJan1() {
-  return <div className='detail-body'>
-  <div className='detail-description'>
-    <img className='detail-photo' alt="Sam1Lam4Zuk3EJan1" src={require(`../images/sam1lam4zuk3ejan1.png`)}></img>
-    <article className='detail-article'>
-      <span className='detail-title'>Sam1Lam4Zuk3EJan1</span><br></br>
-      My first ever <b>Javascript</b> project or even first ever <b>Javascript</b> code. 
-      This school group project contains two parts: a login page, which is coded by my three other groupmates. 
-      And a real-time multi-player Hide-n-Seek game (more like a game of tag to be precise). The game was built 
-      with <b>p5.js</b> for the game mechanics, and <b>SocketIO</b> and <b>Express</b> for server-client communication. 
-      <br></br>
-      In the game, players can either be the Hiders or the Seeker, which the latter one can catch the former one by simply 
-      colliding with them. There are also coins, quest items and revival items for the Hiders to pick up.
-    </article>
+  return <div>
+  <div className='detail-body'>
+    <div className='detail-description'>
+      <img className='detail-photo' alt="Sam1Lam4Zuk3EJan1" src={require(`../images/sam1lam4zuk3ejan1.png`)}></img>
+      <article className='detail-article'>
+        <span className='detail-title'>Sam1Lam4Zuk3EJan1</span><br></br>
+        My first ever <b>Javascript</b> project or even first ever <b>Javascript</b> code. 
+        This school group project contains two parts: a login page, which is coded by my three other groupmates. 
+        And a real-time multi-player Hide-n-Seek game (more like a game of tag to be precise). The game was built 
+        with <b>p5.js</b> for the game mechanics, and <b>SocketIO</b> and <b>Express</b> for server-client communication. 
+        <br></br>
+        In the game, players can either be the Hiders or the Seeker, which the latter one can catch the former one by simply 
+        colliding with them. There are also coins, quest items and revival items for the Hiders to pick up.
+      </article>
+    </div>
+    <div className='detail-info'>
+      <span className='feature'>Features: </span><span className='listoffeatures'><b>real-time multiplayer</b>, <b>client-server</b> ...etc</span><br></br>
+      <span className='related'>Related Tech: </span><span className='techs'>HTML, Javascript, p5.js, Express, SocketIO</span>
+    </div>
+    <br></br>
+    <div className='sources'>
+        <a href='https://github.com/LiKinFai/csci3100project_2022_C7' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
+        <a href='https://github.com/LiKinFai/EditedPDF/blob/main/editedreport.pdf' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="PDF" src={require(`../images/link.png`)}/></a>
+    </div>
   </div>
-  <div className='detail-info'>
-    <span className='feature'>Features: </span><span className='listoffeatures'><b>real-time multiplayer</b>, <b>client-server</b> ...etc</span><br></br>
-    <span className='related'>Related Tech: </span><span className='techs'>HTML, Javascript, p5.js, Express, SocketIO</span>
-  </div>
-  <br></br>
-  <div className='sources'>
-      <a href='https://github.com/LiKinFai/csci3100project_2022_C7' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
-      <a href='https://github.com/LiKinFai/EditedPDF/blob/main/editedreport.pdf' target={'_blank'} rel="noreferrer" ><img className='source-images' alt="PDF" src={require(`../images/link.png`)}/></a>
+  <div className='contact' id='contact'>
+    <div className='contact-body'>
+      <header className='contact-header'>Contact Me</header>
+      <div className='contact-buttons'>
+        <a href='https://github.com/LiKinFai' target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="GitHub" src={require(`../images/github.png`)}/></a>
+        <a href='https://www.linkedin.com/in/%E5%81%A5%E8%BC%9D-%E6%9D%8E-195845259/' target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="LinkedIn" src={require(`../images/linkedin.png`)}/></a>
+        <a href="mailto: likinfaiemail@gmail.com" target={'_blank'} rel="noreferrer" ><img className='contact-images' alt="Email" src={require(`../images/email.png`)}/></a>
+      </div>
+    </div>
   </div>
 </div>
 
